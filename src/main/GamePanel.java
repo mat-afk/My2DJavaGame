f86924240da23d825e3c61e50784edf3551a1672
Mateus <mateusc.ramos@outlook.com>
2023-06-12 01:19:51 -0300

    Tiles e maps

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.plaf.DimensionUIResource;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
     
@@ -16,20 +17,17 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale;  // 48x48
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;  // 768px
-    final int screenHeight = tileSize * maxScreenRow;  // 576px
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;  // 768px
+    public final int screenHeight = tileSize * maxScreenRow;  // 576px
 
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
-
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
 
@@ -46,42 +44,9 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();
     }
 
-    /*
-    @Override
     public void run() {
 
-        double drawInterval = 1000000000 / FPS;  // 0.01666 seconds
-        double nextDrawTime = System.nanoTime() + drawInterval;
-        
-        while(gameThread != null) {
-
-            update();
-
-            repaint();
-
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime / 1000000;
-
-                if(remainingTime < 0) {
-                    remainingTime = 0;
-                }
-
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-    */
-
-    public void run() {
-
-        double drawInterval = 1000000000 / FPS;
+        double drawInterval = 1000000000 / (double) FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
@@ -121,6 +86,8 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+
+        tileM.draw(g2);
 
         player.draw(g2);
 
