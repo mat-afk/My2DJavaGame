1c41cd55f619c52ed288e53beca07a4f75d4d0b8
Mateus <mateusc.ramos@outlook.com>
2023-06-11 21:26:13 -0300

    Transição para IntelliJ

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,6 +3,7 @@ package entity;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 import javax.imageio.ImageIO;
 
 import main.GamePanel;
@@ -24,14 +25,14 @@ public class Player extends Entity {
 
     public void getPlayerImage() {
         try {
-            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right_2.png"));
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,22 +49,22 @@ public class Player extends Entity {
 
     public void update() {
 
-        if(keyH.upPressed == true || keyH.downPressed == true 
-        || keyH.leftPressed == true || keyH.rightPressed == true) {
+        if(keyH.upPressed || keyH.downPressed
+        || keyH.leftPressed || keyH.rightPressed) {
             
-            if(keyH.upPressed == true) { 
+            if(keyH.upPressed) {
                 direction = "up";
                 y -= speed;
             }
-            if(keyH.downPressed == true) {
+            if(keyH.downPressed) {
                 direction = "down";
                 y += speed;
             }
-            if(keyH.leftPressed == true) {
+            if(keyH.leftPressed) {
                 direction = "left";
                 x -= speed;
             }
-            if(keyH.rightPressed == true) {
+            if(keyH.rightPressed) {
                 direction = "right";
                 x += speed;
             }
@@ -88,33 +89,30 @@ public class Player extends Entity {
         BufferedImage image = null;
 
         switch (direction) {
-            case "up":
-                if(spriteNum == 1)
+            case "up" -> {
+                if (spriteNum == 1)
                     image = up1;
-                if(spriteNum == 2)
+                if (spriteNum == 2)
                     image = up2;
-                break;
-
-            case "down":
-                if(spriteNum == 1)
+            }
+            case "down" -> {
+                if (spriteNum == 1)
                     image = down1;
-                if(spriteNum == 2)
+                if (spriteNum == 2)
                     image = down2;
-                break;
-                
-            case "left":
-                if(spriteNum == 1)
+            }
+            case "left" -> {
+                if (spriteNum == 1)
                     image = left1;
-                if(spriteNum == 2)
+                if (spriteNum == 2)
                     image = left2;
-                break;
-            
-            case "right":
-                if(spriteNum == 1)
+            }
+            case "right" -> {
+                if (spriteNum == 1)
                     image = right1;
-                if(spriteNum == 2)
+                if (spriteNum == 2)
                     image = right2;
-                break;
+            }
         }
 
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
--- src/main/Main.java
+++ src/main/Main.java
@@ -20,6 +20,5 @@ public class Main {
         window.setVisible(true);
 
         gamePanel.startGameThread();
-
     }
 }
